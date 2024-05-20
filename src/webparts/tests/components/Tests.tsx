01131d5230f01bfe56377e65e2ecbff830716248
LUIS FERNANDO RODRIGUEZ ORTIZ <rodriguezluf@HILDEBOG.LOCAL>
2024-05-20 17:35:40 -0500

    Modificacion consulta en librerias

--- src/webparts/tests/components/Tests.tsx
+++ src/webparts/tests/components/Tests.tsx
@@ -15,7 +15,8 @@ export default class Tests extends React.Component<ITestsProps, any> {
     this.state = {
       siteLibraries: [],
       libraryFiles: [],
-      documentLink: "",
+      documentInformation: null,
+      unknownLibrary: true
     };
 
     this.getSiteLibraries();
@@ -32,24 +33,44 @@ export default class Tests extends React.Component<ITestsProps, any> {
     }
   };
 
-  private async getLibraryFile(formData:React.FormEvent<HTMLFormElement>): Promise<void> {
+  private searchFile = (formData:React.FormEvent<HTMLFormElement>) => {
     formData.preventDefault();
-
-    const selectedLibrary:string = formData.currentTarget['librarySelector'].value;
     const fileName:string = formData.currentTarget['fileNameInput'].value;
+
+    if(!this.state.siteLibraries || this.state.siteLibraries.length === 0){
+      alert("No se han encontrado librerias en el sitio actual")
+    }
+    else{
+      if(this.state.unknownLibrary && this.state.siteLibraries.length > 0){
+        for (const library of this.state.siteLibraries) {
+          if(library && library.ServerRelativeUrl && library.ServerRelativeUrl.length > 0)
+            this.getLibraryFile(library.ServerRelativeUrl, fileName);
+        }
+
+        if(!this.state.documentInformation || this.state.documentInformation.length === 0){
+          alert("No se encontró el archivo buscado");
+        }
+      }
+      else{
+        const selectedLibraryRelativeUrl:string = formData.currentTarget['librarySelector'].value;
+        this.getLibraryFile(selectedLibraryRelativeUrl, fileName);
+      }
+    }
+  }
+
+  private async getLibraryFile(libraryRelativeUrl:string, fileName:string): Promise<void> {
+    this.setState({documentInformation: null});
     try {
       const documentInfo = await this.pnp.getFileByName(
-        selectedLibrary,
+        libraryRelativeUrl,
         fileName
       );
-      this.setState({documentLink: documentInfo.LinkingUrl});
+      this.setState({documentInformation: documentInfo});
       console.clear();
       console.log("Informacion del archivo:", documentInfo);
       alert(`La información del archivo fue consultada con exito. Para ver el documento, seguir el Link y para ver mayor informacion del documento, dirigirse a la consola.`);
     } catch (error) {
-      this.setState({documentLink: ""});
-      console.error("Error al obtener datos de SharePoint:", error);
-      alert("Error al obtener informacion del archivo o archivo no encontrado");
+      console.error("Error al obtener datos del documento:", error);
     }
   }
 
@@ -57,18 +78,24 @@ export default class Tests extends React.Component<ITestsProps, any> {
   public render(): React.ReactElement<ITestsProps> {
     return (
       <main>
-        <form onSubmit={(form:React.FormEvent<HTMLFormElement>) => this.getLibraryFile(form)}>
+        <form onSubmit={(form:React.FormEvent<HTMLFormElement>) => this.searchFile(form)}>
+          <label htmlFor='unknownLibraryCheck'>Libreria desconocida</label>
+          <input type='checkbox' id='unknownLibraryCheck' value={this.state.unknownLibrary} checked={this.state.unknownLibrary} onChange={() => this.setState({unknownLibrary: !this.state.unknownLibrary})} />
+
           <div>
-            <select name="librarySelector">
-              <option value="" defaultChecked disabled >Seleccionar libreria ...</option>
-              {
-                this.state.siteLibraries.length > 0 && this.state.siteLibraries.map((library:DocumentLibraryInformation) => (
-                  <option key={library.ServerRelativeUrl} value={library.ServerRelativeUrl}>
-                    {library.Title}
-                  </option>
-                ))
-              }
-            </select>
+            {
+              !this.state.unknownLibrary && 
+              <select name="librarySelector">
+                <option value="" defaultChecked >Seleccionar libreria...</option>
+                {
+                  this.state.siteLibraries.length > 0 && this.state.siteLibraries.map((library:DocumentLibraryInformation) => (
+                    <option key={library.ServerRelativeUrl} value={library.ServerRelativeUrl}>
+                      {library.Title}
+                    </option>
+                  ))
+                }
+              </select>
+            }
             <input type="text" name='fileNameInput' placeholder='Ingresar Nombre del documento a consultar' />
           </div>
           <button type='submit'>
@@ -76,13 +103,14 @@ export default class Tests extends React.Component<ITestsProps, any> {
           </button>
         </form>
         {
-          this.state.documentLink && this.state.documentLink.length > 0 &&
-          <>
+          this.state.documentInformation && this.state.documentInformation.LinkingUrl.length > 0 &&
+          <section>
             <br/>
-            <section>
-              <a href={this.state.documentLink} target='_blank'>Abrir documento</a>
-            </section>
-          </>
+            <h4>Se encontró la siguiente información del documento buscado: </h4>
+            <p><b>Nombre del archivo: </b>{this.state.documentInformation.Title}</p>
+            <p><b>Ubicación del archivo: </b>{this.state.documentInformation.ServerRelativeUrl}</p>
+            <a href={this.state.documentInformation.LinkingUrl} target='_blank'>Abrir documento</a>
+          </section>
         }
       </main>
     );
